192d00fffc2595e44b137dd89f8f5c2d2a52663e
fabioFG <fabio.fernando.guerreiro@gmail.com>
2021-12-22 18:55:55 +0000

    navbar mobile friendly - work in progress

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,55 +7,72 @@ import Authentication from "../Authentication/Authentication";
 function Navbar() {
   // Get the value from the context
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
-  
+
+  //Eventlistener for the hamburguer and click
+  /*   const hamburguer = document.querySelector(".hamburguer");
+   */
+
+  /*   window.onload=function(){
+  hamburguer.addEventListener("click", function () {
+    this.classList.toggle('is-active');
+  });
+  } */
   return (
-    <nav className="Navbar">
-      {/* Logo  redirecting to the home page*/}
-      <div>
+    <div className="container">
+      {/* this should be a header */}
+      <header className="header">
         <Link to="/" className="logo">
           <h1>HybridBox</h1>
         </Link>
-      </div>
+        <input type="checkbox" className="nav-toggle" id="nav-toggle" />
+        <label for="nav-toggle" className="nav-toggle-label">
+          <span>X</span>
+        </label>
+        {/* Logo  redirecting to the home page*/}
 
+        <nav className="nav-container">
+          <Link to="/channels" className="link-style">
+            Channels
+          </Link>
 
-      <div className="nav-container">
-        <Link to="/channels" className="link-style">Channels</Link>
+          {!isLoggedIn && (
+            <>
+              <div className="test-wrap">
 
-
-        {!isLoggedIn && (
-          <>
-           <Authentication />
-          </>
-        )}
-
-        {isLoggedIn && (
-          <>
-            <button onClick={logOutUser} className="logout-btn">
-              Logout
-            </button>
-          </>
-        )}
-        
-        <div className="profile-img-wrapper">
-          {user && (
-            <Link to="/profile" className="link-profile">
-              <img
-                className="profile-img"
-                src={user.image}
-                alt="profile"
-                style={{
-                  height: "40px",
-                  width: "40px",
-                  marginTop: "2px"
-                }}
-              />
-            </Link>
+                <Authentication />
+              </div>
+            
+            </>
           )}
 
-        </div>
-     {/*    <i class="fa fa-bars"></i> */}
-      </div>
-    </nav>
+          {isLoggedIn && (
+            <>
+              <button onClick={logOutUser} className="logout-btn">
+                Logout
+              </button>
+            </>
+          )}
+
+          <div className="profile-img-wrapper">
+            {user && (
+              <Link to="/profile" className="link-profile">
+                <img
+                  className="profile-img"
+                  src={user.image}
+                  alt="profile"
+                  style={{
+                    height: "40px",
+                    width: "40px",
+                    marginTop: "2px",
+                  }}
+                />
+              </Link>
+            )}
+          </div>
+          <i className="fa fa-bars" style={{color:"white"}}></i>
+        </nav>
+      </header>
+    </div>
   );
 }
 
